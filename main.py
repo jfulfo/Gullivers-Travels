6336971ee254975e5d2b382c2becd5115f667174
Fulbone <47907450+Fulbone@users.noreply.github.com>
2022-04-23 17:11:48 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+    ______      ____
+   / ____/_  __/ / /_  ____  ____  ___
+  / /_  / / / / / __ \/ __ \/ __ \/ _ \
+ / __/ / /_/ / / /_/ / /_/ / / / /  __/
+/_/    \__,_/_/_.___/\____/_/ /_/\___/
+
+Made by Jake Fulford
+Created on April 23, 2022
+Last Updated April 23, 2022
+
+This project is intended to simulate space travel using
+equations I have learned in my AP Physics class.
+
+Features include realistic motion, time frames, and time dilation.
+
+Project inspired by the satire by Jonathan Swift, Gulliver's Travels
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
